fcb2dc5d4947021f80b69c29c3104d2603699fd0
David Berga <david.berga@eurecat.org>
2022-04-07 17:41:43 +0200

    added script for counting git lines (additions+removals)

---
+++ git_count_lines.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/b‌​in
+if [ -z $1 ]
+then
+echo "You must provide a USER NAME"
+exit N
+else
+USER=$1
+fi
+
+git config --global alias.count-lines "! git log --author=\"\$1\" --pretty=tformat: --numstat | awk '{ add += \$1; subs += \$2; loc += \$1 - \$2 } END { printf \"added lines: %s, removed lines: %s, total lines: %s\n\", add, subs, loc }' #"
+
+git count-lines $USER
