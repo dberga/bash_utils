0a36f0b9717601b7495809026eb50f4ecad4fe93
dberga <dberga@users.noreply.github.com>
2022-05-05 18:42:21 +0200

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,8 +2,8 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/b‌​in
 if [ -z $1 ]
 then
-echo "You must provide a IMAGE_NAME as first argument"
-exit N
+echo "You should provide a IMAGE_NAME as first argument"
+IMAGE_NAME=$(docker images -q | head -n 1)
 else
 IMAGE_NAME=$1
 fi
