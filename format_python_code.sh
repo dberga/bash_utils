d24a733044c4caafbb0043a95581043760d4f6b1
dberga <dberga@users.noreply.github.com>
2024-05-29 13:03:18 +0200

    Update format_python_code.sh

--- format_python_code.sh
+++ format_python_code.sh
@@ -1,12 +1,29 @@
-# Using autopep8
-autopep8 FILE.py --in-place
-autopep8 FOLDER --in-place --recursive
+if [ -z $1 ]
+then
+echo "You must provide a FILE_NAME.py as first argument"
+exit
+else
+FILE=$1
+fi
+if [ -z $2 ]
+then
+echo "You should provide a FOLDER_NAME as second argument if ran on folder"
 
-# Using ruff
-ruff check FILE.py
-ruff check FOLDER
-ruff fix --fix FILE.py --unsafe-fixes
-ruff fix --fix FOLDER --unsafe-fixes
-
-# Using tox (sudo apt-get install tox)
+# Applying to file only
+ruff check $FILE
+autopep8 $FILE --in-place
+ruff fix --fix $FILE --unsafe-fixes
 tox -e reformat
+else
+
+# Applying to folder
+FOLDER=$2
+autopep8 $FOLDER --in-place --recursive
+ruff fix --fix $FOLDER --unsafe-fixes
+tox -e reformat
+fi
+
+# INSTALLATIONS
+# For using autopep8 (pip install autopep8)
+# For using ruff (pip install ruff)
+# For using tox (sudo apt-get install tox)
